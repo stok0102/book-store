6210bf95cb272bc59f06fc127a93e1bea84999ec
Chip Carnes and Caleb Stokka <caleb@>
2016-08-25 15:37:32 -0700

    increment the quantity property of the book model with propertyIncrement

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -11,19 +11,24 @@ export default Ember.Service.extend({
 
     if (this.books.includes(book)) {
       console.log('in cart');
-      book.quantity += 1;
       console.log(book.quantity);
+      // book.quantity ++;
+      // Ember.set(book, 'quantity', 2);
+      book.incrementProperty('quantity');
     } else {
       console.log('empty');
       this.get('books').pushObject(book);
       book.quantity = 1;
     }
+    // this.total();
   },
   remove(book) {
     this.get('books').removeObject(book);
-  },
-
-  includes(book) {
-    return this.get('books').includes(book);
   }
+
+  // total() {
+  //   for (var i = 0; i < this.books.length; i++) {
+  //     this.currentTotal += (this.books[i].quantity * this.books[i].price);
+  //   }
+  // }
 });
